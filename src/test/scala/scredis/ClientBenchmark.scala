3f94429085a60f26e897705c22bf97cac42322db
Alexandre Curreli <alexandre.curreli@livestream.com>
2014-07-28 17:26:30 +0200

    Updated ClientBenchmark

--- src/test/scala/scredis/ClientBenchmark.scala
+++ src/test/scala/scredis/ClientBenchmark.scala
@@ -55,6 +55,53 @@ object ClientBenchmark extends PerformanceTest {
         Await.result(future, 30 seconds)
       }
     }
+    
+    measure method "GET" in {
+      using(sizes) config {
+        exec.maxWarmupRuns -> 3
+        exec.benchRuns -> 3
+        exec.independentSamples -> 3
+      } setUp { _ =>
+        system = ActorSystem()
+        client = Client()(system)
+        Await.result(client.set("foo", "bar"), 2 seconds)
+      } tearDown { _ =>
+        Await.result(client.del("foo"), 2 seconds)
+        Await.result(client.quit(), 2 seconds)
+        system.shutdown()
+        client = null
+        system = null
+      } in { i =>
+        implicit val ec = system.dispatcher
+        val future = Future.traverse(1 to i) { i =>
+          client.get("foo")
+        }
+        Await.result(future, 30 seconds)
+      }
+    }
+    
+    measure method "SET" in {
+      using(sizes) config {
+        exec.maxWarmupRuns -> 3
+        exec.benchRuns -> 3
+        exec.independentSamples -> 3
+      } setUp { _ =>
+        system = ActorSystem()
+        client = Client()(system)
+      } tearDown { _ =>
+        Await.result(client.del("foo"), 2 seconds)
+        Await.result(client.quit(), 2 seconds)
+        system.shutdown()
+        client = null
+        system = null
+      } in { i =>
+        implicit val ec = system.dispatcher
+        val future = Future.traverse(1 to i) { i =>
+          client.set("foo", "bar")
+        }
+        Await.result(future, 30 seconds)
+      }
+    }
   }
   
 }*/
