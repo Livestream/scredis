056a203fbe95c1d1e30e7389db33d989c7e358c4
Justin Kaeser <justin.kaeser@1und1.de>
2015-06-18 14:52:21 +0200

    fix typo

--- start-redis-test-instances.sh
+++ start-redis-test-instances.sh
@@ -9,4 +9,4 @@ redis-server src/test/resources/redis-6380.conf &
 # the redis cluster instances have their own respective directory
 # clean the directory before starting, so we get a fresh cluster
 find src/test/resources/cluster -mindepth 1 -maxdepth 1 -type d -execdir sh -c 'cd {}; rm appendonly.aof dump.rdp nodes.conf; redis-server ./redis.conf &' \;
-redis-trib.rb create --replicas 1 127.0.0.1:7000 127.0.0.1:7001 127.0.0.15:7002 127.0.0.1:7003 127.0.0.1:7004 127.0.0.1:7005
+redis-trib.rb create --replicas 1 127.0.0.1:7000 127.0.0.1:7001 127.0.0.1:7002 127.0.0.1:7003 127.0.0.1:7004 127.0.0.1:7005
